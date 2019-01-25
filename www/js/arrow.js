053012906dec8ce8530f1e5459e01e8eff92e7e5
AlinaK8 <alina.karanda@gmail.com>
2019-01-26 00:45:55 +0200

    hw9

---
+++ www/js/arrow.js
@@ -0,0 +1,15 @@
+//Переделать функцию с использованием функции-стрелки (в методе reduce тоже использовать arrow function):
+
+// function sum() {
+//     const params = Array.prototype.slice.call(arguments);
+//     if (!params.length) return 0;
+//     return params.reduce(function (prev, next) { return prev + next; });
+//   }
+  
+
+const sum = (...args) => {
+  args.reduce((previous, current) => previous + current, 0);
+};
+
+sum(1, 2, 3, 4); // 10
+sum(); // 0
---
+++ www/js/destructuring.js
@@ -0,0 +1,30 @@
+//Task 1
+// Используя rest оператор и деструктуризацию, создать функцию, которая принимает любое количество аргументов и возвращает объект, содержащий первый аргумент и массив из остатка:
+
+// func(‘a’, ‘b’, ‘c’, ‘d’) → 
+// {
+//   first: ‘a’,
+//   other: [‘b’, ‘c’, ‘d’]
+// }
+
+
+function getNewValue(...rest) {
+    if (!rest.length) return "please enter value!"
+    return 
+}
+
+//Task 2
+
+// Организовать функцию getInfo, которая принимает объект вида
+// { name: ...,  info: { employees: [...], partners: [ … ]  } }
+// и выводит в консоль имя (если имени нет, показывать ‘Unknown’) и первые две компании из массива partners:
+
+const organisation = {  
+  name: 'Google',   
+  info: { employees: [‘Vlad’, ‘Olga’], partners: ['Microsoft', 'Facebook', 'Xing']   } 
+};
+// getInfo(organisation); → 
+// Name: Google 
+// Partners: Microsoft Facebook
+
+function getInfo()
